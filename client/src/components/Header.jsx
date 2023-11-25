d96debddbd4b0ff72ba019b99b0486815c73bb85
Matt Doyle <MattDoyleDev@gmail.com>
2023-11-25 17:26:46 -0500

    update to header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,13 +17,13 @@ const Header = ({ title, subtitle }) => {
             fontWeight="bold"
             sx={{ mb: "5px" }}
         >
-            {title} Poop 1 inside Header.jsx
+            {title} inside Header.jsx
         </Typography>
         <Typography
             variant="h5"
             color={theme.palette.secondary[300]}
         >
-            {subtitle} Poop 2 inside Header.jsx
+            {subtitle} inside Header.jsx
         </Typography>
     </Box>
   )
