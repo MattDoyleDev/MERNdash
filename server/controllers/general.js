df2930c761279c78ac86b0873642c8397e5ad1d6
Aaqib925 <muhammadaaqib925@gmail.com>
2024-08-08 22:56:25 +0500

    fixed dashboard api

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import OverallStat from "../models/Transaction.js";
+import OverallStat from "../models/OverallStat.js";
 import Transaction from "../models/Transaction.js";
 
 export const getUser = async (req, res) => {
